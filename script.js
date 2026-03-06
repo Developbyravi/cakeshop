3e9f98b55d1134203666f148d6a70d698035fec4
Ravi khile <kr2932429@gmail.com>
2026-03-06 10:32:25 +0530

    Refactor cart functionality and mobile navigation

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-<!-- Google tag (gtag.js) -->
-async src="https://www.googletagmanager.com/gtag/js?id=G-JD7J69DKEW"
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-JD7J69DKEW');
 
 // Mobile Navigation Toggle
 const hamburger = document.querySelector('.hamburger');
@@ -313,4 +307,5 @@ style.textContent = `
     }
 `;
 document.head.appendChild(style);
+
 
